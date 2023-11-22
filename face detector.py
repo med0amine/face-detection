2b99ebc98328f38e617b5fb6d33b2cd9e4d603f5
med0amine <151380756+med0amine@users.noreply.github.com>
2023-11-22 13:47:01 +0100

    Add files via upload

---
+++ face detector.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python3
+
+__author__ = "Mohamed Amine Ben Ammar"
+__dete__ = "12/08/2022"
+
+
+import cv2
+from imutils.video.pivideostream import PiVideoStream
+import imutils
+import mediapipe as mp
+import time
+import numpy as np
+
+
+class VideoCamera(object):
+    def __init__(self, flip = False):
+        self.vs = PiVideoStream(resolution=(1280, 720), framerate=60).start()
+        self.flip = flip
+        time.sleep(2.0)
+
+    def __del__(self):
+        self.vs.stop()
+
+    def flip_if_needed(self, frame):
+        if self.flip:
+            return np.flip(frame, 0)
+        return frame
+
+    def get_frame(self):
+        frame = self.flip_if_needed(self.vs.read())
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+
+class FaceDetector():
+    def __init__(self, minDetectionCon=0.5):
+
+        self.minDetectionCon = minDetectionCon
+
+        self.mpFaceDetection = mp.solutions.face_detection
+        self.mpDraw = mp.solutions.drawing_utils
+        self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)
+
+    def findFaces(self, img, draw=True):
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceDetection.process(imgRGB)
+        # print(self.results)
+        bboxs = []
+        if self.results.detections:
+            for id, detection in enumerate(self.results.detections):
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+                bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                       int(bboxC.width * iw), int(bboxC.height * ih)
+                bboxs.append([id, bbox, detection.score])
+                img = self.fancyDraw(img,bbox)
+
+                cv2.putText(img, f'{int(detection.score[0] * 100)}%',
+                    (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 255), 2)
+        return img, bboxs
+
+    def fancyDraw(self, img, bbox, l=30, t=5, rt= 1):
+        x, y, w, h = bbox
+        x1, y1 = x + w, y + h
+
+        cv2.rectangle(img, bbox, (255, 0, 255), rt)
+        # Top Left  x,y
+        cv2.line(img, (x, y), (x + l, y), (255, 0, 255), t)
+        cv2.line(img, (x, y), (x, y+l), (255, 0, 255), t)
+        # Top Right  x1,y
+        cv2.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
+        cv2.line(img, (x1, y), (x1, y+l), (255, 0, 255), t)
+        # Bottom Left  x,y1
+        cv2.line(img, (x, y1), (x + l, y1), (255, 0, 255), t)
+        cv2.line(img, (x, y1), (x, y1 - l), (255, 0, 255), t)
+        # Bottom Right  x1,y1
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
+        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), t)
+        return img
+
+
+def main():
+    vc = VideoCamera(flip=False)
+    pTime = 0
+    detector = FaceDetector()
+    while True:
+        success, img = vc.read()
+        img, bboxs = detector.findFaces(img)
+        print(bboxs)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
